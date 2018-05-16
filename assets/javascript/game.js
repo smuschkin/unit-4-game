6507a550ac353012703bea889714946f90dcd1ab
Sara Muschkin <sunshinesara55@MacBook-Pro-2.local>
2018-05-15 20:15:25 -0400

    final game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,64 @@
-//Computer score
-// var computerscore;
-
 var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
-var finalscore;
+var finalscore = 0;
+var computerscore = 0;
+var wins = 0;
+var losses = 0;
 
-$('#computer').on('click', function() {
-    console.log('before click')
-    console.log('1: ' + crystal1 + '2: ' + crystal2 + '3: ' + crystal3 + '4: ' + crystal4)
-    var computerscore = Math.floor((Math.random() * 100));
-    crystal1 = Math.floor((Math.random() * 10));
-    crystal2 = Math.floor((Math.random() * 10));
-    crystal3 = Math.floor((Math.random() * 10));
-    crystal4 = Math.floor((Math.random() * 10));
+function reset(){
     finalscore = 0;
-    console.log('after click')
-    // console.log('1: ' + crystal1 + '2: ' + crystal2 + '3: ' + crystal3 + '4: ' + crystal4)
-    $('#computerScore').html(computerscore);
-    $('#score').html(finalscore);
+};    
 
+$(document).ready(function() {
+
+    $('#computer').on('click', function() {
+        console.log('before click');
+        console.log('1: ' + crystal1 + '2: ' + crystal2 + '3: ' + crystal3 + '4: ' + crystal4);
+        computerscore = Math.floor((Math.random() * 100));
+        crystal1 = Math.floor((Math.random() * 10) +1);
+        crystal2 = Math.floor((Math.random() * 10) +1);
+        crystal3 = Math.floor((Math.random() * 10) +1);
+        crystal4 = Math.floor((Math.random() * 10) +1);
+        console.log('after click')
+        $('#computerscore').html(computerscore);
+        $('#score').html(finalscore);    
+        reset();
     });
 
-//When image1 √is clicked
-$('#crystal1').on('click', function() {
-    // add the value of crystal1 to the total score
-    finalscore += crystal1;
-    console.log(crystal1);
-    $('#score').html(finalscore);
+    $(".add").on("click", function(){
+        var crystalscore = $(this).attr("id");
 
+        if (crystalscore === "crystal11") {
+            finalscore += crystal1;
+        }
+        else if (crystalscore === "crystal22"){
+            finalscore += crystal2;
+        }
+        else if (crystalscore === "crystal33"){
+            finalscore += crystal3;
+        }
+        else if (crystalscore === "crystal44"){
+            finalscore += crystal4;
+        };   
+
+        $('#score').html(finalscore);
+
+
+        if (finalscore === computerscore) {
+            alert("You Win!");
+            wins++;
+            $('#wins').html(wins);
+            reset();
+        }
+        else if (finalscore > computerscore) {
+            alert("You Lose!");
+            losses++;
+            $('#losses').html(losses);
+            reset();
+        }
+    });
 });
 
-//When image2 is clicked
-$('#crystal2').on('click', function() {
-    finalscore += crystal2;
-    console.log(crystal2);
-    $('#score').html(finalscore);
-
-});
-
-$('#crystal3').on('click', function() {
-    finalscore += crystal3;
-    console.log(crystal3);
-    $('#score').html(finalscore);
-
-});
-
-$('#crystal4').on('click', function() {
-    finalscore += crystal4;
-    console.log(crystal4);
-    $('#score').html(finalscore);
-
-});
-
-// function add () {
-//     console.log('1: ' + crystal1 + '2: ' + crystal2 + '3: ' + crystal3 + '4: ' + crystal4)
-//     // finalscore = crystal1 + crystal2 + crystal3 + crystal4;
-//     $('#score').html(finalscore);
-//     console.log(finalscore);
-// };
 
